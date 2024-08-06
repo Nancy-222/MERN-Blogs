966f9d624aa9fd1b131643a340958509c3ec6b21
Marwan A <marwane_akiki@live.com>
2024-08-06 13:31:21 +0300

    Added Upvote Downvote Delete Buttons

---
+++ ToDo.md
@@ -0,0 +1,8 @@
+USERS
+
+Log In
+
+Blog Content Form
+
+Upvote
+Downvote Button
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar =() =>{
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Blog Buddy</h1>
+                    <h1>BlogSphere</h1>
                 </Link>
 
             </div>
