6b8bb52a316727ad358f92e9c6cf668f21361045
yasminelahoud <yasmine.lahoud@net.usj.edu.lb>
2024-08-06 13:18:03 +0300

    Added 4 buttons at the top

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,24 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Ensure this file is updated
 
-const Navbar =() =>{
+const Navbar = () => {
     return (
-        <header>
+        <header className="navbar">
             <div className="container">
-                <Link to="/">
-                    <h1>Blog Buddy</h1>
-                </Link>
-
+                <div className="brand">
+                    <Link to="/">
+                        <h1>Blog Buddy</h1>
+                    </Link>
+                </div>
+                <nav className="nav-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/contact">Contact Us</Link>
+                    <Link to="/signin">Sign In</Link>
+                    <Link to="/login">Log In</Link>
+                </nav>
             </div>
         </header>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
