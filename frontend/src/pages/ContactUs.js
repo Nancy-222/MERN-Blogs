7cd7f1a54b3c213dfa554e245e8e032944c4b411
Nancy-222 <nancy.abi.farah@hotmail.com>
2024-08-07 19:19:26 +0300

    Added required marker to contact us page

--- frontend/src/pages/ContactUs.js
+++ frontend/src/pages/ContactUs.js
@@ -8,17 +8,17 @@ const ContactUs = () => {
       <h2 className="contact-us-heading">Contact Us</h2>
       <Form className="contact-us-form">
         <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>Name<span style={{color: "red"}}>*</span></Form.Label>
           <Form.Control type="text" placeholder="Your Name" />
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>Email address<span style={{color: "red"}}>*</span></Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group controlId="formBasicMessage">
-          <Form.Label>Message</Form.Label>
+          <Form.Label>Message<span style={{color: "red"}}>*</span></Form.Label>
           <Form.Control as="textarea" rows={3} placeholder="Your Message" />
         </Form.Group>
 
