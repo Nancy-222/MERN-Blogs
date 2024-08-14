4c35af803c1430aba067317597cdc064e62940c6
Marwan A <marwane_akiki@live.com>
2024-08-14 14:42:10 +0300

    Updated blogpages.js

--- frontend/src/pages/BlogPages.js
+++ frontend/src/pages/BlogPages.js
@@ -1,118 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import './BlogPages.css'; // Ensure this file exists or correct the path
-import { FiArrowDown, FiArrowUp, FiMessageSquare, FiBookmark } from "react-icons/fi";
+import React, { useEffect, useState } from 'react';
+import { FiArrowUp, FiArrowDown, FiMessageSquare, FiBookmark } from 'react-icons/fi';
+import './BlogPages.css'; // Assuming you have a CSS file for styling
 
 const formatDate = (dateString) => {
-  const options = { hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-  return new Date(dateString).toLocaleDateString('en-US', options);
+    const options = { hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
 };
 
 const BlogsPage = () => {
-  const [showComments, setShowComments] = useState({});
-  const [blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState([]);
+    const [showComments, setShowComments] = useState({});
 
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const response = await fetch('http://localhost:4000/api/blogs');
-        if (!response.ok) throw new Error('Failed to fetch blogs');
-        const data = await response.json();
-        setBlogs(data);
-      } catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const response = await fetch('http://localhost:4000/api/blogs');
+                const data = await response.json();
+                setBlogs(data);
+            } catch (error) {
+                console.error('Error fetching blogs:', error);
+            }
+        };
+        fetchBlogs();
+    }, []);
+
+    const handleCommentFormToggle = (blogId) => {
+        setShowComments(prevState => ({
+            ...prevState,
+            [blogId]: !prevState[blogId]
+        }));
     };
 
-    fetchBlogs();
-  }, []);
+    return (
+        <div className="blogs-page">
+            <div className="blog-container">
+                {blogs.map(blog => (
+                    <div key={blog._id} className="blog-block">
+                        <h4 className="blog-title">{blog.title}</h4>
+                        {blog.image && (
+                            <div className="blog-image">
+                                <img
+                                    src={blog.image}
+                                    alt="Blog"
+                                    className="blog-image-img"
+                                />
+                            </div>
+                        )}
+                        <div className="blog-content">
+                            <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+                        </div>
+                        <p className='posted-on'>Posted On: {formatDate(blog.createdAt)}</p>
+                        <p className="blog-author"><strong>By: {blog.author}</strong></p>
 
-  const fetchComments = async (blogId) => {
-    try {
-      const response = await fetch(`http://localhost:4000/api/blogs/${blogId}/comments`);
-      const data = await response.json();
-      setShowComments((prevState) => ({
-        ...prevState,
-        [blogId]: data
-      }));
-    } catch (error) {
-      console.error("Failed to fetch comments", error);
-    }
-  };
+                        <div className="blog-actions">
+                            <button
+                                title="Upvote"
+                                className="icon-button"
+                            >
+                                <FiArrowUp /> {blog.upvotes}
+                            </button>
+                            <button
+                                title="Downvote"
+                                className="icon-button"
+                            >
+                                <FiArrowDown /> {blog.downvotes}
+                            </button>
 
-  const handleCommentFormToggle = (blogId) => {
-    setShowComments((prevState) => {
-      const newShowComments = { ...prevState };
-      if (newShowComments[blogId]) {
-        delete newShowComments[blogId];
-      } else {
-        fetchComments(blogId);
-        newShowComments[blogId] = [];
-      }
-      return newShowComments;
-    });
-  };
+                            <button
+                                title="Add Comment"
+                                className="icon-button"
+                                onClick={() => handleCommentFormToggle(blog._id)}
+                            >
+                                <FiMessageSquare /> {blog.comments.length}
+                            </button>
+                            
+                            <button
+                                title="Saves"
+                                className="icon-button"
+                            >
+                                <FiBookmark /> {blog.saves}
+                            </button>
+                        </div>
 
-
-
-return (
-  <div className="blogs-page">
-    <div className="blog-container">
-      {blogs.map(blog => (
-        <div key={blog._id} className="blog-block">
-          <h4>{blog.title}</h4>
-          {blog.image && (
-            <div className="blog-image">
-                <img src={`http://localhost:4000/uploads/${blog.image}`} alt="Blog" className="blog-image-img" />
+                        {showComments[blog._id] && (
+                            <div className="comments-list">
+                                {blog.comments.length > 0 ? (
+                                    blog.comments.map(comment => (
+                                        <div key={comment._id} className="comment-item">
+                                            <p><strong>{comment.author}</strong> commented:</p>
+                                            <p>{comment.content}</p>
+                                            <p className="comment-date">{formatDate(comment.createdAt)}</p>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p>No comments yet.</p>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                ))}
             </div>
-        )}
-          <div dangerouslySetInnerHTML={{ __html: blog.content }} />
-          <p className="posted-on">Posted On: {formatDate(blog.createdAt)}</p>
-          <p className="blog-author"><strong>By: {blog.author}</strong></p>
-          <div className="blog-actions">
-            <button className="icon-button" title="Upvote">
-              <FiArrowUp />
-              <span className="icon-value">{blog.upvotes}</span>
-            </button>
-            <button className="icon-button" title="Downvote">
-              <FiArrowDown />
-              <span className="icon-value">{blog.downvotes}</span>
-            </button>
-            <button
-              className="icon-button"
-              title="View Comments"
-              onClick={() => handleCommentFormToggle(blog._id)}
-            >
-              <FiMessageSquare />
-              <span className="icon-value">{blog.comments.length}</span>
-            </button>
-            <button className="icon-button" title="View Saved Blogs">
-              <FiBookmark />
-              <span className="icon-value">{blog.saves || 0}</span>
-            </button>
-          </div>
-
-          {/* Comment List */}
-          {showComments[blog._id] && (
-            <div className="comments-list">
-              {showComments[blog._id].length > 0 ? (
-                showComments[blog._id].map(comment => (
-                  <div key={comment._id} className="comment-item">
-                    <p><strong>{comment.author}</strong> says:</p>
-                    <p>{comment.content}</p>
-                    <p className="comment-date">{formatDate(comment.createdAt)}</p>
-                  </div>
-                ))
-              ) : (
-                <p>No comments yet.</p>
-              )}
-            </div>
-          )}
         </div>
-      ))}
-    </div>
-  </div>
-);
+    );
 };
 
 export default BlogsPage;
-
